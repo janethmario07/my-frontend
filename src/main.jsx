1438ed675619460a2a20ae3ab10bebb141e082bc
janethmario07 <janethmario07@gmail.com>
2025-08-04 15:23:57 +0530

    npm installed again

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import { store } from './store/Store.js';
+//import { store } from './store/Store.js';
 import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AuthProvider } from "./contexts/AuthProvider";
@@ -15,7 +15,7 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   // <StrictMode>
 
-      <Provider store={store}>
+      <Provider >
         <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <AuthProvider>
